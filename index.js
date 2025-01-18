6efbc5b23378fc7a1bceb0cb9926a6ea86a158f7
Emran Ahmed <emran.bd.08@gmail.com>
2025-01-18 23:38:39 +0600

    update files

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ module.exports = {
       'lint:css:report': 'npm run lint:css -- --output-file scss-report.txt',
       'lint:css:fix': 'npm run lint:css -- --fix',
 
-      'lint:js': 'wp-scripts lint-js \'./src/**/*.js\'',
+      'lint:js': 'wp-scripts --format=pretty lint-js \'./src/**/*.js\'',
       'lint:js:report': 'npm run lint:js -- --format html --output-file lint-report.html',
       'lint:js:fix': 'npm run lint:js -- --fix',
 
@@ -120,6 +120,7 @@ module.exports = {
       'fs-extra',
       'webpack-remove-empty-scripts',
       'eslint-plugin-prettier',
+      'eslint-formatter-pretty@5',
     ],
     customPackageJSON: {
       'lint-staged': {
